c41223261af4e1e0cdfc77e80068d51ed0de8024
Dakota hall <Hallda020292@gmail.com>
2022-09-22 16:30:06 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string mydogName = "leo";
+            int mydogAge = 2;
+            char mydogmiddleName = 'b';
+            bool Heisthebestdog = true;
+            Double mydogfoodcost = 39.99;
+
+
+
+            Console.WriteLine($"hello my dog's name is {mydogName}+{mydogmiddleName},{mydogName} is {mydogAge} years old");
+            Console.WriteLine($"Leo {true} but his food costs {mydogfoodcost} it is expensive!");
+
+
+        }
+    }
+}
